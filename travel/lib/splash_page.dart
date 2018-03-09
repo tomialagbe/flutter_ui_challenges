3d8e65f303ecd655f1cc02fe6e1621cd43592d33
Tomi Alagbe <tomialagbe@yahoo.com>
2018-03-09 22:09:00 +0100

    Fixed Issues in Challenge 2
    - Improved splash screen animation
    - Fixed issue with sliding image

--- travel/lib/splash_page.dart
+++ travel/lib/splash_page.dart
@@ -1,59 +1,227 @@
 import 'dart:async';
 import 'package:flutter/scheduler.dart' show timeDilation;
 import 'package:flutter/material.dart';
-import 'package:travel/stacked_circles.dart';
+import 'package:travel/circles.dart';
 
 class SplashPage extends StatefulWidget {
   @override
   State createState() => new SplashPageState();
 }
 
-class SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+class SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
 
   final circleDiameter = 50.0;
-  AnimationController _controller;
-  Animation<Alignment> alignmentAnimation;
+
+  AnimationController controller1;
+  Animation<double> translation1;
+  Animation<double> scale1;
+  Animation<double> translation1Reverse;
+  Animation<double> scale1Reverse;
+
+  Animation<double> translation2;
+  Animation<double> scale2;
+  Animation<double> translation2Reverse;
+  Animation<double> scale2Reverse;
+
+  Animation<double> translation3;
+  Animation<double> scale3;
+  Animation<double> translation3Reverse;
+  Animation<double> scale3Reverse;
+
+  Animation<double> translation4;
+
+  double currentTranslation = 0.0;
+  double currentScale = 1.0;
 
   @override
   void initState() {
     super.initState();
-    _controller = new AnimationController(vsync: this, duration: const Duration(milliseconds: 2000));
-    _playStackedCirclesAnimation();
+
+    controller1 =
+    new AnimationController(vsync: this, duration: const Duration(seconds: 7));
+    translation1 = new Tween<double>(begin: 50.0, end: -20.0).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.0, 0.14, curve: Curves.easeInOut)));
+    scale1 = new Tween<double>(begin: 0.8, end: 0.4).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.0, 0.14, curve: Curves.linear)));
+
+    translation1Reverse = new Tween<double>(begin: -20.0, end: 55.0).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.14, 0.28, curve: Curves.easeInOut)));
+    scale1Reverse = new Tween<double>(begin: 0.4, end: 0.85).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.14, 0.28, curve: Curves.linear)));
+
+    translation2 = new Tween<double>(begin: 55.0, end: -20.0).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.28, 0.42, curve: Curves.easeInOut)));
+    scale2 = new Tween<double>(begin: 0.85, end: 0.4).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.28, 0.42, curve: Curves.linear)));
+
+    translation2Reverse = new Tween<double>(begin: -20.0, end: 60.0).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.42, 0.57, curve: Curves.easeInOut)));
+    scale2Reverse = new Tween<double>(begin: 0.4, end: 0.9).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.42, 0.57, curve: Curves.linear)));
+
+    translation3 = new Tween<double>(begin: 60.0, end: -20.0).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.57, 0.71, curve: Curves.easeInOut)));
+    scale3 = new Tween<double>(begin: 0.9, end: 0.4).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.57, 0.71, curve: Curves.linear)));
+
+    translation3Reverse = new Tween<double>(begin: -20.0, end: 65.0).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.71, 0.85, curve: Curves.easeInOut)));
+    scale3Reverse = new Tween<double>(begin: 0.4, end: 1.0).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.71, 0.85, curve: Curves.linear)));
+
+    translation4 = new Tween<double>(begin: 65.0, end: 15.0).animate(
+        new CurvedAnimation(parent: controller1,
+            curve: new Interval(0.85, 1.0, curve: Curves.linear)));
+
+    translation1.addListener(() {
+      setCurrentTranslation(translation1);
+    });
+    scale1.addListener(() {
+      setCurrentScale(scale1);
+    });
+
+    translation1Reverse.addListener(() {
+      if (translation1.value == -20) {
+        setCurrentTranslation(translation1Reverse);
+      }
+    });
+
+    scale1Reverse.addListener(() {
+      if (scale1.value == 0.4) {
+        setCurrentScale(scale1Reverse);
+      }
+    });
+
+    translation2.addListener(() {
+      if (translation1Reverse.value == 55) {
+        setCurrentTranslation(translation2);
+      }
+    });
+    scale2.addListener(() {
+      if (scale1Reverse.value == 0.85) {
+        setCurrentScale(scale2);
+      }
+    });
+
+    translation2Reverse.addListener(() {
+      if (translation2.value == -20) {
+        setCurrentTranslation(translation2Reverse);
+      }
+    });
+
+    scale2Reverse.addListener(() {
+      if (scale2.value == 0.4) {
+        setCurrentScale(scale2Reverse);
+      }
+    });
+
+    translation3.addListener(() {
+      if (translation2Reverse.value == 60) {
+        setCurrentTranslation(translation3);
+      }
+    });
+    scale3.addListener(() {
+      if (scale2Reverse.value == 0.9) {
+        setCurrentScale(scale3);
+      }
+    });
+
+    translation3Reverse.addListener(() {
+      if (translation3.value == -20) {
+        setCurrentTranslation(translation3Reverse);
+      }
+    });
+
+    scale3Reverse.addListener(() {
+      if (scale3.value == 0.4) {
+        setCurrentScale(scale3Reverse);
+      }
+    });
+
+    translation4.addListener(() {
+      if (translation3Reverse.value == 65) {
+        setCurrentTranslation(translation4);
+      }
+    });
+
+    controller1.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        showLandingPage();
+      }
+    });
+
+    controller1.forward();
+  }
+
+  void setCurrentTranslation(Animation<double> animation) {
+    setState(() {
+      currentTranslation = animation.value;
+    });
+  }
+
+  void setCurrentScale(Animation<double> scaleAnimation) {
+    setState(() {
+      currentScale = scaleAnimation.value;
+    });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    controller1.dispose();
     super.dispose();
   }
 
-  Future<Null> _playStackedCirclesAnimation() async {
-    try {
-      await _controller.forward().orCancel;
-      await _controller.reverse().orCancel;
-      await _controller.forward().orCancel;
-      await _controller.reverse().orCancel;
-      await _controller
-          .animateTo(0.400)
-          .orCancel;
-      await Navigator.of(context).pushNamed("/landing");
-    } on TickerCanceled {
-      //animation cancelled
-    }
-  }
   @override
   Widget build(BuildContext context) {
-    timeDilation = 1.0;
+    final matrix1 = new Matrix4.translationValues(
+        -1 * currentTranslation, 0.0, 0.0);
+    matrix1.scale(currentScale, currentScale);
+    final matrix2 = new Matrix4.translationValues(currentTranslation, 0.0, 0.0);
+    matrix2.scale(currentScale, currentScale);
+
     return new Scaffold(
       body: new Center(
         child: new Hero(
           tag: "CircleHeroTag",
-          child: new StackedCirclesAnimation(
-            controller: _controller.view,
+          child: new Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              new Transform(
+                transform: matrix1,
+                child: new Circle(
+                    color: Colors.purple, diameter: circleDiameter),
+              ),
+              new Transform(
+                transform: matrix2,
+                child: new Opacity(
+                  opacity: 0.8,
+                  child: new Circle(
+                      color: Colors.yellow, diameter: circleDiameter),
+                ),
+              )
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void showLandingPage() {
+    new Future.delayed(new Duration(milliseconds: 500)).then((_) =>
+        Navigator.of(context).pushNamed("/landing"));
   }
 
 }
--- travel/lib/stacked_circles.dart
+++ travel/lib/stacked_circles.dart
@@ -4,11 +4,44 @@ import 'package:travel/circles.dart';
 class StackedCircles extends StatelessWidget {
 
   final double circleDiameter;
+  final Matrix4 circle1Transform;
+  final Matrix4 circle2Transform;
 
-  StackedCircles(this.circleDiameter);
+  StackedCircles(this.circleDiameter,
+      {this.circle1Transform, this.circle2Transform});
 
   @override
   Widget build(BuildContext context) {
+//    final matrix1 = circle1Transform ?? new Matrix4.translationValues(
+//        -15.0, 0.0, 0.0)
+//      ..scale(0.5, 0.5);
+//    final matrix2 = circle2Transform ??
+//        new Matrix4.translationValues(15.0, 0.0, 0.0)
+//          ..scale(0.5, 0.5);
+//
+//    return new SizedBox(
+//      width: circleDiameter * 2,
+//      height: circleDiameter,
+//      child: new Stack(
+//        alignment: Alignment.center,
+//        children: <Widget>[
+//          new Transform(
+//            transform: matrix1,
+//            child: new Circle(
+//                color: Colors.purple, diameter: circleDiameter),
+//          ),
+//          new Transform(
+//            transform: matrix2,
+//            child: new Opacity(
+//              opacity: 0.8,
+//              child: new Circle(
+//                  color: Colors.yellow, diameter: circleDiameter),
+//            ),
+//          )
+//        ],
+//      ),
+//    );
+
     return new SizedBox(
       width: circleDiameter * 2,
       height: circleDiameter,
@@ -37,25 +70,29 @@ class StackedCirclesAnimation extends StatelessWidget {
   final Animation<double> diameter;
   final Animation<Alignment> alignment, alignment1;
 
-  StackedCirclesAnimation({Key key, this.controller}):
+  StackedCirclesAnimation({Key key, this.controller})
+      :
         diameter = new Tween<double>(
             end: 25.0,
             begin: 30.0
-        ).animate(new CurvedAnimation(parent: controller, curve: new Interval(0.0, 1.0, curve: Curves.ease))),
+        ).animate(new CurvedAnimation(parent: controller,
+            curve: new Interval(0.0, 1.0, curve: Curves.ease))),
 
-      alignment = new AlignmentTween(
+        alignment = new AlignmentTween(
           begin: new Alignment(-0.5, 0.0),
           end: new Alignment(0.2, 0.0),
-        ).animate(new CurvedAnimation(parent: controller, curve: new Interval(0.0, 1.0, curve: Curves.ease))),
+        ).animate(new CurvedAnimation(parent: controller,
+            curve: new Interval(0.0, 1.0, curve: Curves.ease))),
 
         alignment1 = new AlignmentTween(
           begin: new Alignment(0.5, 0.0),
           end: new Alignment(-0.2, 0.0),
-        ).animate(new CurvedAnimation(parent: controller, curve: new Interval(0.0, 1.0, curve: Curves.ease))),
+        ).animate(new CurvedAnimation(parent: controller,
+            curve: new Interval(0.0, 1.0, curve: Curves.ease))),
 
         super(key: key);
 
-  Widget _buildAnimation(BuildContext context, Widget child){
+  Widget _buildAnimation(BuildContext context, Widget child) {
     return new Stack(
       children: <Widget>[
         new Align(
@@ -72,6 +109,7 @@ class StackedCirclesAnimation extends StatelessWidget {
       ],
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return new AnimatedBuilder(
