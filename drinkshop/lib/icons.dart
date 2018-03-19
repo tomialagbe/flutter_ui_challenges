86c21d804c4b4d511bcd926df2660cdd4e2e9ea3
Braulio Cassule <brauliocassule94@gmail.com>
2018-03-19 02:06:37 +0100

    add new image and font assets

--- drinkshop/lib/icons.dart
+++ drinkshop/lib/icons.dart
@@ -5,4 +5,6 @@ class DrinkShopIcons {
   static final IconData plus = new IconData(0xf217, fontFamily: fontFamily);
   static final IconData controls = new IconData(0xf39d, fontFamily: fontFamily);
   static final IconData menu = new IconData(0xf20d, fontFamily: fontFamily);
+  static final IconData snowflake = new IconData(0xe80c, fontFamily: "fontello");
+  static final IconData iceCubes = new IconData(0xe80b, fontFamily: "fontello");
 }
