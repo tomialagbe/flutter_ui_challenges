eea7e7099e5f9d85e54e2cb344ada0a6054f461e
Braulio Cassule <brauliocassule94@gmail.com>
2018-04-18 16:31:53 +0100

    fix books layout

--- bookreader/lib/book_row.dart
+++ bookreader/lib/book_row.dart
@@ -2,17 +2,16 @@ import 'package:bookreader/book.dart';
 import 'package:flutter/material.dart';
 
 class BookRow extends StatelessWidget {
-
   PageController pageController = new PageController(viewportFraction: 0.85);
 
   @override
   Widget build(BuildContext context) {
-    return  new Container(
+    return new Container(
       height: 380.0,
       width: double.infinity,
       child: new PageView(
           controller: pageController,
-          children: books.map((Book book){
+          children: books.map((Book book) {
             return Padding(
               padding: EdgeInsets.all(10.0),
               child: new Container(
@@ -21,32 +20,58 @@ class BookRow extends StatelessWidget {
                   shape: BoxShape.rectangle,
                   borderRadius: new BorderRadius.circular(10.0),
                   boxShadow: <BoxShadow>[
-                    new BoxShadow(color: Colors.black38,
+                    new BoxShadow(
+                        color: Colors.black38,
                         blurRadius: 2.0,
                         spreadRadius: 1.0,
-                        offset: new Offset(0.0, 1.0)),
+                        offset: new Offset(0.0, 2.0)),
                   ],
                 ),
                 child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     new Container(
-                      height: 250.0,
+                      height: 220.0,
                       width: double.infinity,
-                      child: new Image.asset(book.asset, fit: BoxFit.cover),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(topRight: Radius.circular(10.0), topLeft: Radius.circular(10.0)),
+                        child: new Image.asset(book.asset, fit: BoxFit.cover),
+                      )
                     ),
-                    new Text(book.title, style: const TextStyle(fontSize: 25.0), textAlign: TextAlign.right),
-                    new Text(book.author),
-                    new RaisedButton(
-                        color: Colors.blue[900],
-                        onPressed: null,
-                        child: new Text('Read Book'),
+                    new Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20.0, bottom: 10.0, top: 10.0),
+                      child: new Text(book.title,
+                          style: const TextStyle(fontSize: 25.0),
+                          textAlign: TextAlign.right),
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.only(left: 20.0, bottom: 10.0),
+                      child: new Text(book.author),
+                    ),
+                    new Container(
+                      margin: const EdgeInsets.only(left: 20.0),
+                      decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(20.0)),
+                      child: new ClipRRect(
+                        borderRadius: new BorderRadius.circular(50.0),
+                        child: new MaterialButton(
+                          minWidth: 70.0,
+                          onPressed: () {},
+                          color: Colors.blue[900],
+                          child: new Text('Read Book',
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17.0,
+                                  fontWeight: FontWeight.w500)),
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
             );
-          }).toList()
-      ),
+          }).toList()),
     );
   }
 }
